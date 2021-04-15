69f6016a040f191c96de2b479cd68d5b8edf604c
Sachin Nitish <sachinnithishk@gmail.com>
2021-04-15 19:29:36 +0530

    Game Pages & UI
    + Added Minecraft Page
    + Added Valheim Page
    + Added Minecraft Sidebar Images
    + Added Valheim Sidebar Images
    + Added generic game page UI
    ~ Changed Home Page layout

--- src/app/components/minecraft/minecraftController.js
+++ src/app/components/minecraft/minecraftController.js
@@ -1,12 +1,21 @@
 angular
   .module("app")
-  .controller("minecraftController", function ($scope, userService) {
+  .controller("minecraftController", function ($scope, $http, userService) {
     // window.api.receive("launchMinecraftMumbleResponse", (data) => {
     //   console.log(`Received ${data} from main process`);
 
     //   $scope.isInstalled = data.status;
     // });
     // window.api.send("launchMinecraftMumbleRequest", "some data");
+
+    $http
+      .get(
+        "https://plan.sain.host/v1/serverOverview?server=de07eaca-346a-4c3b-ae62-8a75004da1f9"
+      )
+      .then(function (response) {
+        $scope.game = response.data;
+        console.log($scope.game);
+      });
 
     $scope.isInstalled = true;
     $scope.isDownloading = true;
